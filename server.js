b6f26726ed7541d8ede99c9b3c186652c281f0da
MuhammadJawaar <muhammadjawaar18@gmail.com>
2023-11-25 20:07:24 +0700

    first commit

---
+++ server.js
@@ -0,0 +1,21 @@
+const express = require('express');
+const cors = require('cors'); // Untuk mengatasi masalah CORS
+
+const app = express();
+const port = 3001; // Sesuaikan port sesuai keinginan
+
+app.use(cors()); // Menggunakan middleware CORS
+
+const servants = require('./servant.json');
+const war = require('./war.json') // Ganti dengan path ke file JSON Anda
+
+app.get('/api/servants', (req, res) => {
+  res.json(servants);
+});
+app.get('/api/war', (req, res) => {
+    res.json(war);
+  });
+
+app.listen(port, () => {
+  console.log(`Server running at http://localhost:${port}`);
+});
